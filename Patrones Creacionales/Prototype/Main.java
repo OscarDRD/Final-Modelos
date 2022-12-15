0e3c5f85058d29980672c1ef1776a79cd884cc50
sebas03b <91993732+sebas03b@users.noreply.github.com>
2022-12-14 22:34:03 -0500

    Create Main.java

---
+++ Patrones Creacionales/Prototype/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Prototipo> menus = new ArrayList<Prototipo>();
+        Prototipo prototipoMenu = new Menu();
+        
+        System.out.println("Ingrese la cantidad de menus a producir");
+        int num = sc.nextInt();
+        for(int i  = 0; i < num; i++){
+            menus.add(prototipoMenu.clonar());
+        }
+        System.out.println(menus.size()+" menus producidos");
+    } 
+}
