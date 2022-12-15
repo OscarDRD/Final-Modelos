382eaf3fe98c66dd29aae7a04e26c821d4e15eea
sebas03b <91993732+sebas03b@users.noreply.github.com>
2022-12-14 22:46:04 -0500

    Create Director.java

---
+++ Patrones Creacionales/Builder/Director.java
@@ -0,0 +1,21 @@
+package builderhamburguesa;
+
+public class Director {
+    private HamburguesaBuilder builder;
+    
+    public void buildHamburguesa(){
+        builder.crearHamb();
+        builder.buildPan();
+        builder.buildCarne();
+        builder.buildSalsas();
+        builder.buildAdiciones();
+    }
+    
+    public void setBuilder(HamburguesaBuilder builder){
+        this.builder = builder;
+    }
+    
+    public Hamburguesa getHamburguesa(){
+        return builder.getHamb();
+    }
+}
