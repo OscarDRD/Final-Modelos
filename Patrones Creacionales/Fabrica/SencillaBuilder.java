4191b7c8d20b64ce22cb128fb44dd4494eab85d0
sebas03b <91993732+sebas03b@users.noreply.github.com>
2022-12-14 22:53:12 -0500

    Create SencillaBuilder.java

---
+++ Patrones Creacionales/Fabrica/SencillaBuilder.java
@@ -0,0 +1,25 @@
+package fabrica;
+
+public class SencillaBuilder extends HamburguesaBuilder {
+
+    @Override
+    public void buildPan() {
+        hamburguesa.setPan("pan con ajonjoli");
+    }
+
+    @Override
+    public void buildSalsas() {
+        hamburguesa.setSalsas("salsa de tomate, mayonesa");
+    }
+
+    @Override
+    public void buildCarne() {
+        hamburguesa.setCarne("carne de res");
+    }
+
+    @Override
+    public void buildAdiciones() {
+        hamburguesa.setAdiciones("lechuga, tomate");
+    }
+    
+}
