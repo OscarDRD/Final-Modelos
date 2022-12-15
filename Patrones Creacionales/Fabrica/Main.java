49b3eddead291e47fe49cc16c0920b53ab444c3e
sebas03b <91993732+sebas03b@users.noreply.github.com>
2022-12-14 22:47:39 -0500

    Create Main.java

---
+++ Patrones Creacionales/Fabrica/Main.java
@@ -0,0 +1,24 @@
+package fabrica;
+import java.util.Scanner;
+/**
+ *
+ * @author sebas
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Fabrica fabrica = new FabricaHamb();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Hamburguesas disponibles:\n1)Sencilla\n2)Vegetariana\nIngrese la opcion deseada:");
+        int op = sc.nextInt();
+        
+        switch(op){
+            case 1:
+                System.out.println(""+fabrica.getHamb("sencilla").getDetalles());
+                break;
+            case 2: 
+                System.out.println(""+fabrica.getHamb("vegetariana").getDetalles());
+                break;
+        }
+    }
+}
