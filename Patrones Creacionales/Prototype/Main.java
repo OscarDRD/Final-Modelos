c402495ce44a5d24170f2514366dedae8caec086
sebas03b <91993732+sebas03b@users.noreply.github.com>
2022-12-14 22:35:20 -0500

    Update Main.java

--- Patrones Creacionales/Prototype/Main.java
+++ Patrones Creacionales/Prototype/Main.java
@@ -1,3 +1,7 @@
+package prototipo;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
